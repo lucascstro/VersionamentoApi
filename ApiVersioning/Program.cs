3f919e497ad7be415dc3bbeebe71812e974b6010
Lucas Castro <lucas.castro@metadados.com.br>
2026-07-03 22:13:17 -0300

    feat: versionamento via query string.

--- ApiVersioning/Program.cs
+++ ApiVersioning/Program.cs
@@ -7,23 +7,24 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        
+
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
 
         builder.Services.AddApiVersioning(options =>
-            {
-                options.DefaultApiVersion = new ApiVersion(1, 0);
-                options.AssumeDefaultVersionWhenUnspecified = true;
-                options.ApiVersionReader = ApiVersionReader.Combine(
-                    //new HeaderApiVersionReader("api-version"),
-                    new QueryStringApiVersionReader("v")
-                );
-            })
-            .AddMvc();
+        {
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                //new HeaderApiVersionReader("api-version"),
+                //new QueryStringApiVersionReader("v")
+                new QueryStringApiVersionReader("api-version")
+            );
+        })
+        .AddMvc();
 
         var app = builder.Build();
-        
+
         app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
